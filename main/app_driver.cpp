089994d8b64a38899e3f86cfc9b072f306d7a862
Jules Rodrigues <jules.rodrigues11@gmail.com>
2024-02-21 16:10:41 +0000

    Initial commit

---
+++ main/app_driver.cpp
@@ -0,0 +1,243 @@
+/*
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <esp_log.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <esp_matter.h>
+#include "bsp/esp-bsp.h"
+
+#include <app_priv.h>
+
+using namespace chip::app::Clusters;
+using namespace esp_matter;
+
+static const char *TAG = "app_driver";
+extern uint16_t light_endpoint_id;
+
+static uint8_t s_led_state = 0;
+static led_strip_handle_t led_strip;
+
+void blink_led(void)
+{
+    ESP_LOGI(TAG, "Blink");
+    /* If the addressable LED is enabled */
+    if (s_led_state) {
+        /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
+        led_strip_set_pixel(led_strip, 0, 16, 16, 16);
+        /* Refresh the strip to send data */
+        led_strip_refresh(led_strip);
+    } else {
+        /* Set all LED off to clear all pixels */
+        led_strip_clear(led_strip);
+    }
+}
+
+void configure_led(void)
+{
+    ESP_LOGI(TAG, "Configured");
+    /* LED strip initialization with the GPIO and pixels number*/
+    led_strip_config_t strip_config = {
+        .strip_gpio_num = BLINK_GPIO,
+        .max_leds = 1, // at least one LED on board
+    };
+    led_strip_rmt_config_t rmt_config = {
+        .resolution_hz = 10 * 1000 * 1000, // 10MHz
+    };
+    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
+    /* Set all LED off to clear all pixels */
+    led_strip_clear(led_strip);
+}
+
+/* Do any conversions/remapping for the actual value here */
+static esp_err_t app_driver_light_set_power(led_indicator_handle_t handle, esp_matter_attr_val_t *val)
+{
+#if BSP_LED_NUM > 0
+    esp_err_t err = ESP_OK;
+    if (val->val.b) {
+        err = led_indicator_start(handle, BSP_LED_ON);
+    } else {
+        err = led_indicator_start(handle, BSP_LED_OFF);
+    }
+    return err;
+#else
+    ESP_LOGI(TAG, "LED set power: %d", val->val.b);
+    blink_led();
+    return ESP_OK;
+#endif
+}
+
+static esp_err_t app_driver_light_set_brightness(led_indicator_handle_t handle, esp_matter_attr_val_t *val)
+{
+    int value = REMAP_TO_RANGE(val->val.u8, MATTER_BRIGHTNESS, STANDARD_BRIGHTNESS);
+#if BSP_LED_NUM > 0
+    return led_indicator_set_brightness(handle, value);
+#else
+    ESP_LOGI(TAG, "LED set brightness: %d", value);
+    return ESP_OK;
+#endif
+}
+
+static esp_err_t app_driver_light_set_hue(led_indicator_handle_t handle, esp_matter_attr_val_t *val)
+{
+    int value = REMAP_TO_RANGE(val->val.u8, MATTER_HUE, STANDARD_HUE);
+#if BSP_LED_NUM > 0
+    uint32_t hsv = led_indicator_get_hsv(handle);
+    SET_HUE(hsv, value);
+    return led_indicator_set_hsv(handle, hsv);
+#else
+    ESP_LOGI(TAG, "LED set hue: %d", value);
+    return ESP_OK;
+#endif
+}
+
+static esp_err_t app_driver_light_set_saturation(led_indicator_handle_t handle, esp_matter_attr_val_t *val)
+{
+    int value = REMAP_TO_RANGE(val->val.u8, MATTER_SATURATION, STANDARD_SATURATION);
+#if BSP_LED_NUM > 0
+    uint32_t hsv = led_indicator_get_hsv(handle);
+    SET_SATURATION(hsv, value);
+    return led_indicator_set_hsv(handle, hsv);
+#else
+    ESP_LOGI(TAG, "LED set saturation: %d", value);
+    return ESP_OK;
+#endif
+}
+
+static esp_err_t app_driver_light_set_temperature(led_indicator_handle_t handle, esp_matter_attr_val_t *val)
+{
+    uint32_t value = REMAP_TO_RANGE_INVERSE(val->val.u16, STANDARD_TEMPERATURE_FACTOR);
+#if BSP_LED_NUM > 0
+    return led_indicator_set_color_temperature(handle, value);
+#else
+    ESP_LOGI(TAG, "LED set temperature: %ld", value);
+    return ESP_OK;
+#endif
+}
+
+static void app_driver_button_toggle_cb(void *arg, void *data)
+{
+
+    ESP_LOGI(TAG, "Toggle button pressed");
+    ESP_LOGI(TAG, "Here");
+    uint16_t endpoint_id = light_endpoint_id;
+    uint32_t cluster_id = OnOff::Id;
+    uint32_t attribute_id = OnOff::Attributes::OnOff::Id;
+
+    node_t *node = node::get();
+    endpoint_t *endpoint = endpoint::get(node, endpoint_id);
+    cluster_t *cluster = cluster::get(endpoint, cluster_id);
+    attribute_t *attribute = attribute::get(cluster, attribute_id);
+
+    esp_matter_attr_val_t val = esp_matter_invalid(NULL);
+    attribute::get_val(attribute, &val);
+    val.val.b = !val.val.b;
+    attribute::update(endpoint_id, cluster_id, attribute_id, &val);
+
+}
+
+esp_err_t app_driver_attribute_update(app_driver_handle_t driver_handle, uint16_t endpoint_id, uint32_t cluster_id,
+                                      uint32_t attribute_id, esp_matter_attr_val_t *val)
+{
+    esp_err_t err = ESP_OK;
+    if (endpoint_id == light_endpoint_id) {
+        led_indicator_handle_t handle = (led_indicator_handle_t)driver_handle;
+        if (cluster_id == OnOff::Id) {
+            if (attribute_id == OnOff::Attributes::OnOff::Id) {
+                err = app_driver_light_set_power(handle, val);
+            }
+        } else if (cluster_id == LevelControl::Id) {
+            if (attribute_id == LevelControl::Attributes::CurrentLevel::Id) {
+                err = app_driver_light_set_brightness(handle, val);
+            }
+        } else if (cluster_id == ColorControl::Id) {
+            if (attribute_id == ColorControl::Attributes::CurrentHue::Id) {
+                err = app_driver_light_set_hue(handle, val);
+            } else if (attribute_id == ColorControl::Attributes::CurrentSaturation::Id) {
+                err = app_driver_light_set_saturation(handle, val);
+            } else if (attribute_id == ColorControl::Attributes::ColorTemperatureMireds::Id) {
+                err = app_driver_light_set_temperature(handle, val);
+            }
+        }
+    }
+    return err;
+}
+
+esp_err_t app_driver_light_set_defaults(uint16_t endpoint_id)
+{
+    esp_err_t err = ESP_OK;
+    void *priv_data = endpoint::get_priv_data(endpoint_id);
+    led_indicator_handle_t handle = (led_indicator_handle_t)priv_data;
+    node_t *node = node::get();
+    endpoint_t *endpoint = endpoint::get(node, endpoint_id);
+    cluster_t *cluster = NULL;
+    attribute_t *attribute = NULL;
+    esp_matter_attr_val_t val = esp_matter_invalid(NULL);
+
+    /* Setting brightness */
+    cluster = cluster::get(endpoint, LevelControl::Id);
+    attribute = attribute::get(cluster, LevelControl::Attributes::CurrentLevel::Id);
+    attribute::get_val(attribute, &val);
+    err |= app_driver_light_set_brightness(handle, &val);
+
+    /* Setting color */
+    cluster = cluster::get(endpoint, ColorControl::Id);
+    attribute = attribute::get(cluster, ColorControl::Attributes::ColorMode::Id);
+    attribute::get_val(attribute, &val);
+    if (val.val.u8 == (uint8_t)ColorControl::ColorMode::kCurrentHueAndCurrentSaturation) {
+        /* Setting hue */
+        attribute = attribute::get(cluster, ColorControl::Attributes::CurrentHue::Id);
+        attribute::get_val(attribute, &val);
+        err |= app_driver_light_set_hue(handle, &val);
+        /* Setting saturation */
+        attribute = attribute::get(cluster, ColorControl::Attributes::CurrentSaturation::Id);
+        attribute::get_val(attribute, &val);
+        err |= app_driver_light_set_saturation(handle, &val);
+    } else if (val.val.u8 == (uint8_t)ColorControl::ColorMode::kColorTemperature) {
+        /* Setting temperature */
+        attribute = attribute::get(cluster, ColorControl::Attributes::ColorTemperatureMireds::Id);
+        attribute::get_val(attribute, &val);
+        err |= app_driver_light_set_temperature(handle, &val);
+    } else {
+        ESP_LOGE(TAG, "Color mode not supported");
+    }
+
+    /* Setting power */
+    cluster = cluster::get(endpoint, OnOff::Id);
+    attribute = attribute::get(cluster, OnOff::Attributes::OnOff::Id);
+    attribute::get_val(attribute, &val);
+    err |= app_driver_light_set_power(handle, &val);
+
+    return err;
+}
+
+app_driver_handle_t app_driver_light_init()
+{
+//configure_led();
+#if BSP_LED_NUM > 0
+    /* Initialize led */
+    led_indicator_handle_t leds[BSP_LED_NUM];
+    ESP_ERROR_CHECK(bsp_led_indicator_create(leds, NULL, BSP_LED_NUM));
+    led_indicator_set_hsv(leds[0], SET_HSV(DEFAULT_HUE, DEFAULT_SATURATION, DEFAULT_BRIGHTNESS));
+    
+    return (app_driver_handle_t)leds[0];
+#else
+    return NULL;
+#endif
+}
+
+app_driver_handle_t app_driver_button_init()
+{
+    /* Initialize button */
+    button_handle_t btns[BSP_BUTTON_NUM];
+    ESP_ERROR_CHECK(bsp_iot_button_create(btns, NULL, BSP_BUTTON_NUM));
+    ESP_ERROR_CHECK(iot_button_register_cb(btns[0], BUTTON_PRESS_DOWN, app_driver_button_toggle_cb, NULL));
+    
+    return (app_driver_handle_t)btns[0];
+}
